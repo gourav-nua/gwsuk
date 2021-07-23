e7147b24cb8c6b82d4b0006aed7c8b63b61121e3
Dilip Gajera <dilip@zealousweb.com>
2021-07-23 12:15:03 +0530

    design chnages

--- dev/tools/grunt/configs/themes.js
+++ dev/tools/grunt/configs/themes.js
@@ -50,5 +50,15 @@ module.exports = {
             'css/styles'
         ],
         dsl: 'less'
-    }
+    },
+    theme: {
+        area: 'frontend',
+        name: 'Nua/theme',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
 };
